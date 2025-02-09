d94805c835c7fb1e9c65d6bc96a57c189f46dbc1
smsl24 <samsulmaariparipin@gmail.com>
2025-02-10 06:41:44 +0700

    first commit

---
+++ src/tugaspertemuan1/Tugaspertemuan1.java
@@ -0,0 +1,9 @@
+package tugaspertemuan1;
+
+public class Tugaspertemuan1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo World Samsul Maarip Aripin 2300134 Saya Senang Belajar Java");
+    }
+    
+}
